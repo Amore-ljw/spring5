ba24cfa5e0e756d60cb44fd256dacc71086a8da2
李佳伟 <lijiawei@opencode.cloud>
2020-07-11 15:11:25 +0800

    测试提交单个文件

---
+++ src/test/java/com/amore/spring5/singleton/EnumSingletonTest.java
@@ -0,0 +1,41 @@
+package com.amore.spring5.singleton;
+
+import com.amore.spring5.pattern.singleton.register.EnumSingleton;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+
+public class EnumSingletonTest {
+
+    public static void main(String[] args) {
+        //测试反射实例化枚举类
+        EnumSingleton instance = EnumSingleton.INSTANCE;
+//        System.out.println(instance.name() + " : " + instance.ordinal());
+//        try {
+//            Class<EnumSingleton> clazz = EnumSingleton.class;
+//            Constructor<EnumSingleton> constructor = clazz.getDeclaredConstructor(String.class,int.class);
+//            constructor.setAccessible(true);
+//            EnumSingleton newInstance = constructor.newInstance();
+//            //Constructor -> newInstance(...) -> if ((clazz.getModifiers() & Modifier.ENUM) != 0)
+//            //throw new IllegalArgumentException("Cannot reflectively create enum objects");
+//            System.out.println(instance == newInstance);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        //测试反序列化创建枚举实例
+        try {
+            //序列化到文件
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("D:/enum.obj"));
+            out.writeObject(instance);
+            out.flush();
+            out.close();
+            //反序列化
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("D:/enum.obj"));
+            EnumSingleton newInstance = (EnumSingleton) in.readObject();
+            System.out.println(instance == newInstance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
